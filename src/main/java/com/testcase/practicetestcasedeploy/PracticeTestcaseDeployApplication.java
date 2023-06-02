64ae0e9cecbf69720119f9b77a58270005c7a7ea
osukhyun00 <osh123chm@gmail.com>
2023-06-02 22:19:57 +0900

    create project

---
+++ src/main/java/com/testcase/practicetestcasedeploy/PracticeTestcaseDeployApplication.java
@@ -0,0 +1,13 @@
+package com.testcase.practicetestcasedeploy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PracticeTestcaseDeployApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PracticeTestcaseDeployApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/testcase/practicetestcasedeploy/PracticeTestcaseDeployApplicationTests.java
@@ -0,0 +1,13 @@
+package com.testcase.practicetestcasedeploy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PracticeTestcaseDeployApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
